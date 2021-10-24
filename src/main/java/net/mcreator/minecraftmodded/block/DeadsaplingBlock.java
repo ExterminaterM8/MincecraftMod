518d8f6240a1ac894fbbdf4b4a074e3e575a0a0d
ExterminaterM8 <Aiden@DESKTOP-C053EP6.modem>
2021-10-24 14:43:09 +0800

    Wicked

--- src/main/java/net/mcreator/minecraftmodded/block/DeadsaplingBlock.java
+++ src/main/java/net/mcreator/minecraftmodded/block/DeadsaplingBlock.java
@@ -2,16 +2,11 @@
 package net.mcreator.minecraftmodded.block;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.biome.BiomeColors;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.FoliageColors;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
@@ -36,7 +31,6 @@ public class DeadsaplingBlock extends MinecraftmoddedModElements.ModElement {
 	public static final Block block = null;
 	public DeadsaplingBlock(MinecraftmoddedModElements instance) {
 		super(instance, 103);
-		FMLJavaModLoadingContext.get().getModEventBus().register(new BlockColorRegisterHandler());
 	}
 
 	@Override
@@ -51,20 +45,10 @@ public class DeadsaplingBlock extends MinecraftmoddedModElements.ModElement {
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getTranslucent());
 	}
-	private static class BlockColorRegisterHandler {
-		@OnlyIn(Dist.CLIENT)
-		@SubscribeEvent
-		public void blockColorLoad(ColorHandlerEvent.Block event) {
-			event.getBlockColors().register((bs, world, pos, index) -> {
-				return world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefault();
-			}, block);
-		}
-	}
-
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
-					.setOpaque((bs, br, bp) -> false));
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.doesNotBlockMovement().notSolid().setOpaque((bs, br, bp) -> false));
 			setRegistryName("deadsapling");
 		}
 
